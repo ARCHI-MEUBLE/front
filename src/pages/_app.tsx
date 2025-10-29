b04a4f8ef965e5166cd0b06392709de772d7dc33
collins <collins.lemogo-kamnen@student.junia.com>
2025-10-29 06:16:46 +0100

    Fix: Add CustomerProvider and model-viewer script to _app.tsx
    - Wrap app with both CustomerProvider and SampleCartProvider
    - Load model-viewer script globally for 3D configurator
    - Fixes 'useCustomer must be used within a CustomerProvider' error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { SampleCartProvider } from "@/contexts/SampleCartContext";
+import { CustomerProvider } from "@/context/CustomerContext";
 import "../../styles/globals.css";
 import "../styles/configurator.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SampleCartProvider>
-      <Component {...pageProps} />
-    </SampleCartProvider>
+    <CustomerProvider>
+      <SampleCartProvider>
+        {/* Load model-viewer globally once for entire app */}
+        <Script
+          type="module"
+          src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"
+          strategy="afterInteractive"
+          onError={(e) => {
+            console.error('Erreur chargement model-viewer:', e);
+          }}
+        />
+        <Component {...pageProps} />
+      </SampleCartProvider>
+    </CustomerProvider>
   );
 }
