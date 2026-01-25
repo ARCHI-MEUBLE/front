c4745c0a0fab215cfe3e924523e3bfe4105fec8c
collins732 <collins.lemogo-kamnen@student.junia.com>
2026-01-26 00:57:44 +0100

    Ajout favicon avec logo ArchiMeuble

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import Head from "next/head";
 import { useEffect } from "react";
 import { SampleCartProvider } from "@/contexts/SampleCartContext";
 import { CustomerProvider } from "@/context/CustomerContext";
@@ -49,19 +50,25 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <CustomerProvider>
-      <SampleCartProvider>
-        {/* Load model-viewer globally once for entire app */}
-        <Script
-          type="module"
-          src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"
-          strategy="afterInteractive"
-          onError={(e) => {
-            console.error('Erreur chargement model-viewer:', e);
-          }}
-        />
-        <Component {...pageProps} />
-      </SampleCartProvider>
-    </CustomerProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/images/logo.jpg" />
+        <link rel="apple-touch-icon" href="/images/logo.jpg" />
+      </Head>
+      <CustomerProvider>
+        <SampleCartProvider>
+          {/* Load model-viewer globally once for entire app */}
+          <Script
+            type="module"
+            src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"
+            strategy="afterInteractive"
+            onError={(e) => {
+              console.error('Erreur chargement model-viewer:', e);
+            }}
+          />
+          <Component {...pageProps} />
+        </SampleCartProvider>
+      </CustomerProvider>
+    </>
   );
 }
